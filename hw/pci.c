xenserver/qemu-xen-4.2

commit 60b80e3ee319e908069d1603e5b73f815acdffac

passthrough: magic ioport protocol no longer unplugs passthrough NICs
On Fri, 12 Feb 2010, Zhai, Edwin wrote:
> [bugs:]
>
> 1. Pass-through NICs are also unplugged, although them have different
>    path with vnif and emulated NIC.

You are right, that is a bug and this patch should fix it.

Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

hw/pci.c
@@ -858,7 +858,8 @@ void pci_unplug_netifs(void)
            dev = bus->devices[x];
            if (dev &&
                dev->config[0xa] == 0 &&
-               dev->config[0xb] == 2) {
+               dev->config[0xb] == 2 &&
+               test_pci_slot(x >> 3) != 1) {
                /* Found a netif.  Remove it from the bus.  Note that
                   we don't free it here, since there could still be
                   references to it floating around.  There are only

solooboroten/xen-device-model

commit 5d86a6cc4ddd48c491329f3a6a5136b95b7f1dbe

passthrough: magic protocol passthrough fix no-passthrough compile
Changeset 60b80e3ee319e908069d1603e5b73f815acdffac broke the build
when CONFIG_PASSTHROUGH was not set.

It also failed to #include a necessary header file, generating a
compiler warning.

Signed-off-by: Ian Jackson <ian.jackson@eu.citrix.com>

committer: Ian Jackson <Ian.Jackson@eu.citrix.com>
git-commit-id: f187eeb8546405d57eb421cdd329d804ee734d23

hw/pci.c
@@ -31,6 +31,10 @@
 #include "exec-all.h"
 #include "qemu-xen.h"
 
+#ifdef CONFIG_PASSTHROUGH
+#include "hw/pass-through.h"
+#endif
+
 extern int igd_passthru;
 
 //#define DEBUG_PCI
@@ -858,8 +862,11 @@ void pci_unplug_netifs(void)
            dev = bus->devices[x];
            if (dev &&
                dev->config[0xa] == 0 &&
-               dev->config[0xb] == 2 &&
-               test_pci_devfn(x) != 1) {
+               dev->config[0xb] == 2
+#ifdef CONFIG_PASSTHROUGH
+               && test_pci_devfn(x) != 1
+#endif
+               ) {
                /* Found a netif.  Remove it from the bus.  Note that
                   we don't free it here, since there could still be
                   references to it floating around.  There are only
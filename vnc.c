a062e236144b998687f2aa19e302946db4209df6
Aleksey Avdeev <avdeev@altell.ru>
2014-02-21 16:02:28 +0400

    patch qemu-5d95ac5b6475c3b6b0e36b5f04de49bba88b3e59

--- vnc.c
+++ vnc.c
@@ -1098,8 +1098,8 @@ static void vnc_disconnect_finish(VncState *vs)
     if (!vs->vd->clients)
         dcl->idle = 1;
 
-    qemu_free(vs);
     vnc_remove_timer(vs->vd);
+    qemu_free(vs);
 }
 
 #ifdef CONFIG_VNC_TLS
